78c0db98e91ed58b66394d5390f20c2fca014d4e
AsyncDefu <nailisuagamedev@gmail.com>
2025-12-18 02:32:03 +0200

    Update GameManager.cs

--- Assets/SCE22/Code/GameManager.cs
+++ Assets/SCE22/Code/GameManager.cs
@@ -1,10 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using SimpleReactive;
 using UnityEngine;
 
 public sealed class GameManager : SingletonMonoBehaviour<GameManager>
 {
     [field: SerializeField] public string GameSaveName { get; private set; } = "dev_test";
+
+    [SerializeField, Space(5)] private ReactiveVar<GameState> _state = new(GameState.GamePlay);
+    public IReadOnlyReactiveVar<GameState> State => _state;
+
 
     public void Save(string saveName = null)
     {
@@ -20,6 +26,8 @@ public sealed class GameManager : SingletonMonoBehaviour<GameManager>
     }
     private IEnumerator LoadEnumerator(GameSave save)
     {
+        _state.Value = GameState.Loading;
+
         Map.Singleton.Clear();
 
         yield return null;
@@ -27,6 +35,14 @@ public sealed class GameManager : SingletonMonoBehaviour<GameManager>
         Map.Singleton.Load(save.Entities);
         Map.Singleton.RefreshReferences(save.Entities);
         Map.Singleton.PostRefreshReferences(save.Entities);
+
+        _state.Value = GameState.GamePlay;
     }
 }
 
+public enum GameState : byte
+{
+    GamePlay,
+    Saving,
+    Loading
+}
